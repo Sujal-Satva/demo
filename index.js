faf2ee71dd65a097564ec2b0200c97c458941c53
Sujal-Satva <sujal@satvasolutions.com>
2025-06-24 10:59:48 +0530

    solve

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+
 
 const app = express();
 
